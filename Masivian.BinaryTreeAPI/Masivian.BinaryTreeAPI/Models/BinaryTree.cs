16379221c77699d7d115dee68c090958fb101998
Roberto Alvarado <ralvaradot@hotmail.com>
2019-10-06 11:55:23 -0500

    Se sube  el proyecto Inicial al repo con los modelos

---
+++ Masivian.BinaryTreeAPI/Masivian.BinaryTreeAPI/Models/BinaryTree.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Masivian.BinaryTreeAPI.Models
+{
+    /// <summary>
+    /// Clase del Arbol Binario
+    /// </summary>
+    public class BinaryTree
+    {
+        /// <summary>
+        /// Id del arbol
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// nombre del arbol
+        /// </summary>
+        public string TreeName { get; set; }
+        /// <summary>
+        /// Numero de Nodos
+        /// </summary>
+        public int TotalNodes { get; set; }
+        /// <summary>
+        /// Arbol de Nodos
+        /// </summary>
+        public List<Node> Nodes { get; set; }
+    }
+}
---
+++ Masivian.BinaryTreeAPI/Masivian.BinaryTreeAPI/Models/Node.cs
@@ -0,0 +1,29 @@
+﻿namespace Masivian.BinaryTreeAPI.Models
+{
+    /// <summary>
+    /// Modelo que representa un Nodo del Arbol Binario
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// Id del Nodo
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Nombre del Nodo o Descripcion
+        /// </summary>
+        public string NodeName { get; set; }
+        /// <summary>
+        /// Id del Nodo padre. Cero si es el padre
+        /// </summary>
+        public int IdFatherNode { get; set; }
+        /// <summary>
+        /// Id Nodo derecho
+        /// </summary>
+        public int IdRightNode { get; set; }
+        /// <summary>
+        /// Id Nodo  Derecho
+        /// </summary>
+        public int IdLeftNode { get; set; }
+    }
+}
